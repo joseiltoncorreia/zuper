2d4a46876aa87107ee0021cd15c5fbc1e5783f08
Joseilton Correia <joseilton.correia@siedos.com.br>
2024-09-02 08:29:54 -0300

    Mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome',
+    overwrite: true,
+    html: true,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,15 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import addContext from 'mochawesome/addContext'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+Cypress.on('test:after:run', (test, runnable) => {
+    if (test.state === 'failed') {
+      const screenshot = `assets/<span class="math-inline">\{Cypress\.spec\.name\}/</span>{runnable.parent.title}   
+   -- ${test.title} (failed).png`;
+      addContext({ test }, screenshot);   
+  
+    }
+  })
